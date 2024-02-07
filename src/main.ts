ef6acccdc9e43451d0364fa529776406e93d20dd
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-02-07 23:03:20 +0300

    chore: get port from environment

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+const port = process.env.port || 3000;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api/v1");
+
+    console.log(
+        `Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`,
+    );
     await app.listen(3000);
 }
 bootstrap();
