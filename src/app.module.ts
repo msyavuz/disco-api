7f7ac014f0d4a07ec75a47b59fba4ec975b05496
Mehmet Salih Yavuz <salih.yavuz@proton.me>
2024-02-10 16:35:12 +0300

    fix: migrate to npm and turso

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,16 @@ import { ActorsModule } from "./actors/actors.module";
 import { Actor } from "./actors/entities/actor.entity";
 import { DentriesModule } from "./dentries/dentries.module";
 import { Dentry } from "./dentries/entities/dentry.entity";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
     imports: [
+        ConfigModule.forRoot(),
         TypeOrmModule.forRoot({
-            type: "better-sqlite3",
-            database: "disco.db",
+            type: "sqlite",
+            database: `libsql://still-green-goblin-msyavuz.turso.io?authToken=${process.env.TOKEN}`,
+            driver: require("@libsql/sqlite3"),
+            flags: 0x00000040,
             entities: [Actor, Dentry],
             synchronize: false,
         }),
